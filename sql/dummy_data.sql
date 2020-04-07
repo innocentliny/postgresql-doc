390b1bf636d0cae1e2f28d7aab5f14abdb50c335
anderson.lin <anderson.lin@asuscloud.com>
2020-04-07 18:26:15 +0800

    initial version.

---
+++ sql/dummy_data.sql
@@ -0,0 +1,197 @@
+INSERT INTO public.user_file ("content") VALUES
+('資金')
+,('hello world! 2010年保障房建設在全國範圍內獲全面啟動，從中央到地方紛紛加大 了保障房的建設和投入力度 。2011年，保障房進入了更大規模的建設階段。住房城鄉建設部黨組書記、部長姜偉新去年底在全國住房城鄉建設工作會議上表示，要繼續推進保障性安居工程建設。')
+,('完全防水紋身，且可無水貼上 皮膚，能輕鬆享有刺青效果，防水紋身貼紙系列皆可維持一周，最真實，質感有如真實刺青！ 實用')
+,('殺菌效果，汞殺菌紫外線可以殺死90％以上的細菌，讓牙刷牙膏也成為裝置藝術，裝置在浴室中！')
+,('有一個工人跑去向他的老闆說：老闆，我愛上你的女兒了，我想跟她交往看看，請你允許。')
+,('前年4/1，我們推出了大受好評的 #小米時光機')
+,('如果今天傳奇副本難度沒那麼高，
+我相信用BUG來遊玩的玩家會是少數
+沒人想看自己支持的遊戲走下坡
+但看看敵人精美的護甲，那RPG等級的攻擊力
+近乎無CD的技能施放頻率，各種鷹眼、麒麟臂
+在敵人面前特工們簡直形同裸體
+「千辛萬苦打半血，一招回到解放前。」
+就是傳奇副本的寫照
+正常通關還得花個2小時以上，
+好不容易打過了，那精美的掉落率簡直感人
+我玩遊戲是為了舒壓，
+不是為了被一群星際大戰的風暴兵
+用近乎藐視的尼哥槍法壓在地上磨擦的
+想體驗被強暴的感覺我不如去玩CSGO
+起碼我被強暴的原因是因為技術不如人
+而不是因為開發商失能導致的
+
+希望開發商在討論BUG帶來的負面影響之前
+先想想為什麼絕大部分玩家會如此積極的使用
+還有連沒使用BUG的玩家也被誤鎖
+UBI我真的是佩服的五體投地🤣
+況且現在遊戲的競爭很激烈
+UBI這次的舉動會不會讓全境大地震我不曉得
+但大部分人不會在支持UBI的遊戲我是肯定的')
+,('MAZDA LEXUS SUBARU TOYOTA等高耗電車款的車友有福了~~
+在加倍伏，您的電瓶選擇可以更多元!!! 以不變更車輛零件下，體驗德製歐規電瓶!!!
+
+由於歐洲車"怠速熄火系統"的普及，使得歐規AGM與EFB電池的銷量大增，價格也逐漸下修，反觀日系車款搭配AGM與EFB電池的車款甚少，高階品牌價格落不下來，客戶除了平價韓製商品可選外，通常沒有更好的AGM商品可以選擇，或是EFB價格直逼德製AGM的價格
+(EFB是AGM降低成本而開發的產物，新車銷售時不能用降成本當藉口，而衍生AGM怕熱的傳聞，事實是所有的電池都怕熱，但是相同熱度下AGM壽命仍遠勝過EFB)
+
+由於全球化策略，越來越多嚴苛充放電系統的日系車款已使用歐規電瓶，除了歐洲生產線是因為地緣關係裝歐規電瓶，亞洲生產線亦考慮歐規電瓶有齊全規格與品質穩定的優勢。例如2017年後豐田集團大幅導入歐規電瓶為新車標準規格
+
+歐製AGM與EFB電瓶更是生產線長年修正品管下的穩定商品(歐洲AGM生產線運作10年以上，EFB生產線運作5年以上)
+
+因此加倍伏提供使用日規電瓶的車友們亦有體驗歐規AGM/EFB電瓶的選擇，只要您是D23 D26 D31 不限正負方向R/L，均可體驗歐製或徳製AGM/EFB電池的品質~~
+
+(以怠速熄火系統車款/與嚴苛充放電系統的日系車款為推薦)
+(改裝料件均為現場安裝贈品，無須加購)
+
+此概念為車友要求而生，經過加倍伏長久觀察評估給予好評，因此公開服務項目滿足日系車友體驗歐規AGM/EFB需求，
+最早案例為FORESTER原廠EFB電瓶兩年內壞，改歐規EXIDE 80AH AGM 至今已使用4年仍持續使用中(當然價格落差很多，只是提供了更多的高階商品滿足車友的升級需求)')
+,('各位夥伴好
+中華大學將在春假結束後的4月11日(六)再次由「Oracle高階資料庫管理碩士在職學位學程」辦理「Oracle專業實務增能活動」，本次活動在 Oracle Academy 的支持下很榮幸的以 Oracle Volunteer 的身分邀請Oracle Taiwan 甲骨文進階服務部的李家瑞資深經理(Gary Li)以「甲骨文雲戰略自治資式料庫」為題，向各位夥伴介紹運行於Oracle雲端的自治資料庫，精彩可期。因應新冠肺炎疫情本次活動使用 MS Teams 以遠距會議的方式進行，活動時間為下午13:10-15:00。
+本次活動報名網址為 https://s.chu.edu.tw/R.aspx?rd=sC3SQy 。歡迎各位夥伴參加這個難得的專業增能活動。
+
+Oracle 高階資料庫管理碩士在職學位學程，是國內首個面對在職朋友，由Oracle與中華大學合作，並以「讓您的資料庫變強」為目標的「職能、證照、學位」三合一的高效率能力導向碩士在職專班，如果您想更深入的了解「Oracle 高階資料庫管理碩士在職學位學程」，歡迎造訪我們的網頁(http://madm.chu.edu.tw)，或是在4月11日(六)活動之後同樣以遠距的方式與我們交流。交流活動將由「Oracle 高階資料庫管理碩士在職學位學程」的學程主任李之中副教授主持。李博士於中華大學曾經輔導超過1000人次中華大學學生取得 Oracle 資料管理證照，並於今年(2020年)獲得 Oracle Academy 的推薦為電子季刊的風雲人物(成功故事即將於 Oracle Academy 官網刊登)。')
+,('廠牌：國產翔穩軍品
+價格：素色新品記得大約兩千多，二手價1500元。或是再議價^_^
+顏色：軍綠色
+狀態：久放大約二年，只有市區休閒外出背過幾次。無髒污，無汗臭，無損傷。
+製造：中華民國/台灣
+所在地：台北市
+其他備註：運費自付或北市面交，連結為官網新品https://www.shop2000.com.tw/J-TECH/product/p465588')
+;
+INSERT INTO public.user_file ("content") VALUES
+('【打開罐頭的那瞬間 讓十萬狗狗都驚呆了！】
+行銷學常說:
+「要用使用者的角度去體會， 因為付錢是大爺」
+（好吧～我承認後面那句是我說的。）
+所以 ，我在購買狗罐頭時，
+一定會看著罐頭包裝，
+用我家狗的心態去揣摩。
+當我拿起牛肉罐頭，
+包裝上大大塊的冒煙牛排照片，
+卻寫著牛肉風味？！
+牛肉風味?這詞太含糊了吧？
+說穿了就是有牛肉味道的果凍而已 ！
+當我拿起雞肉罐頭，
+包裝上肥美多汁的雞肉圖案，
+卻寫著雞肉口味？!
+但我非常清楚明瞭，裡面沒雞肉，
+就跟老婆餅裡沒老婆一樣令人失望。
+將心比心後，
+我終於能明白，
+每次我買了【風味或口味系列】的罐頭回家，
+狗狗那一臉無奈的神情是怎麼回事!
+直到我今天去了好市多，
+走過寵物食品區的時候，
+瞥了一眼這罐頭，
+走了幾步又倒回來……
+這罐頭包裝和我以往看到的完全不一樣！
+上面寫著 【牛肉蔬菜 】（雖然蔬菜令人討厭）
+但是上面的圖案
+是一整碗滿滿牛肉塊，和一點點蔬菜（這很重要）
+看了真的很吸引人！
+雖然擔心買回去
+也可能是風味系列銘謝惠顧沒有肉的罐頭~
+但我還是決定放手一搏，
+而且想好最壞打算，
+如果買回去大黑不賞臉，
+我退貨原因會寫:「狗狗說他不滿意。」
+買回家要開罐頭的時候，
+我像往常一樣隨便傾斜拿著開，
+殊不知!在打開的那瞬間，肉汁流到我手上，
+我傻眼，映入眼簾的是滿的滿滿肉塊!
+跟圖片上一模一樣！
+「哼 !都是幻覺~ 嚇不了我低~」
+被騙習慣的我， 相信絕對沒這麼好康！
+肉下面一能是果凍或其他液態填充物，
+就跟洋芋片打開，空氣比洋芋片多一樣的道理。
+我把整罐倒出來，
+肉塊像咖哩一樣源源不絕傾瀉而出，
+香味四溢，閃著金色的光芒。
+只差沒有小當家的神龍飛過!
+我家的狗快瘋了！
+站起來一直催促我 快點給我吃！！！
+我拿著罐頭驚呼
+「這根本真材實料! CP值也太高了吧！」
+不誇張，罐頭瞬間秒殺，
+狗狗大口吃肉吃的很爽！
+重點是，
+這滿滿肉塊的罐頭24罐竟然只要849元！！
+我真心覺得佛心來的！
+吃過的都讚不絕口 ！回味無窮！魂牽夢縈！
+不信你看照片！')
+,('剛剛想到，就來提一下前陣子跟人閒聊的，我之前常講技術細節，怎樣會影響到戰略層級的事情。由於現代戰略實在太複雜，這邊講個例子讓人容易懂。
+假設台灣以前只有100枚雄三，射程號稱300km，命中率據說很高。發射器2枚一組共50組，且我們全部裝在陸地上，若車載一台2組4枚，1個部隊2台共4支部隊，總計32枚，其他68枚放在固定陣地。
+這狀況會是，中國要打過來，固定陣地一定要解決，且假設他們透過各種辦法知道你陣地在哪裡。先不算CEP，就假設這固定陣地算4枚一組，用整數算15組需要中國砸15x4=60枚彈道飛彈才能確保摧毀。
+車載的到處跑，必須派飛機搜索與攻擊，若先採取空中打擊，則要考慮台灣對空武力。所以我們就先假設中國要不管死傷，重型武器滿載登陸艇全數衝過來好了。32枚反艦飛彈，假設我們算8枚同時打一艘登陸艦才能擊沉，共軍需要4台以上才能確定登陸成功。
+然後因為中士的努力，讓我們發現雄三很好用，實際命中率高得嚇人，平均只要2枚就可以打爆一艘，保險點4枚一艘好了，所以決定增產到300枚。
+這會發生何事？
+用同樣比例去算，代表固定陣地需要180枚彈道飛彈才能保證摧毀。車載96枚，理論上可以擊沉大型登陸艦24艘，所以要確定強攻台灣能夠成功，就得要準備這個數量以上的艦艇，以及足夠數量攻陷台灣的軍隊。
+再說一次喔，這還沒有包括先搶制空權好幾天，搶到後還得派一堆戰機護航攻擊機，慢慢找出不知躲在哪裡的車載雄三。故，若中國只有20台大小登陸艦，要保證至少8成16艘可以登陸成功(一般外國軍棋假定)，就得確保雄三被打到剩下8發以下。
+==============
+好了，這是很粗淺的講法，我的意思是，有任何人提出一些很神奇的百萬舢舨渡海論，都得要提出實際的數字。像是宣稱可以派出幾千架飛機飽和對台攻擊，我會問你的油料要準備多少，放在哪些地方，你預期每一波攻擊，解放軍可以打掉多少台灣戰力，自己戰損多少......等等。
+不然基本上都是在談解放軍無敵論的信仰。
+這些還沒算科技的東西進去，你保有幾個巡弋飛彈量，零件更換幾年一次。也沒算天候海像的修正，好比搭過AP艦的就知道台灣海峽海像有多「親民」。更沒有把作戰需要的基本物資囤積與消耗量算進去，以及士兵登艦裝載跟來回時間考慮進去。
+我們業餘的當然不專業，專業的都是拿電腦跑一堆參數，但即使如此就夠讓你知道，台灣要被攻下，一點都不簡單。
+===============
+不過，歷史上中國對外戰爭，沒有一次是做好準備才打，而是自己想打就會打，打贏打輸都在其次，能不能對內宣傳神勇無敵，保住自己權位才是重點。
+故，我們看待可能的台海戰爭，很少說死到一定不會。')
+,('車泊多林檢查所與阿里山遊樂區遊玩介紹（開放資料自取）+小故事。
+今日車泊奮起湖附近多林檢查所，果然是夜市車泊點，無時無刻來都有車泊人員在此駐紮，車泊共分兩區，一區呈現長方形停車場，一區呈現正方形停車場，我區人員非常優良，晚上都很安靜，沒有開門關門聲音，社友品質很優，水廁方面沒優算勉強可以，今日車泊車約有20多台吧。
+一早起來當然是觀賞眾多車友的車型拉，有轎車，小客車，露營車，貨車，福斯系列車，得力卡，斯貝斯基，海力士，皮卡，，，，各類車種在此一次滿足。
+一早起來看多人鑑賞皮卡改裝車，於是好奇也去鑑賞，稍微詢問疑惑點，一社友就口氣很差對我說，這要對車很內行才能問，心中默默忍下，我很想說（現在是專業時代，不是年紀大就一定比年輕人懂），我稍微看了一下只問主人一題，得到如下資料。
+1）儲水系統是用6吋水管製成.並各自相通且由下層先進水，下層滿水後才往上層續水，水位最高處有一排氣孔，方便檢式水是否滿。
+2）進水方面採用幫浦進水，故需要排氣控，以免造成背壓無法進水。
+3）進水處左側安裝一般過濾器。
+4）右上方喝水用採用逆滲透法。
+5）左側採用12000正逆變器，輸入線徑用22平方線，採紅正黑負顏色，輸入為12V，輸出為ac220以減少損耗（一般會採用ac110v，開關採用陶瓷閘刀開關75A250型。
+6）中間為12/24v共用型小冰箱。
+7）右側有外國進口的手動式熱水器。
+8）帳篷可以升降，採用鋼索式升降。
+9）太陽能板為300瓦，單矽晶片組，滑軌採用商規三段式滑組，故能板略有下垂現象，開路電壓約40V
+短路電流約9安培，最大功率電壓約30V以上，最大功率電流約9安培。
+以上觀察結果，老大哥你也在旁邊看比我久，上面資料您看到嗎？我可以問車主問題了嗎，現在是專業時代，不是年紀時代，請相信現在年輕人的專業，不是年紀大就可以叫人閉嘴。
+順提供一份自己去阿里山遊樂區旅遊雜記，資料如下請自行取用。
+日出，神木，阿里山
+1.遊樂區門票 $300/人，24小時收費。
+2.祝山火車單程$150/人，來回$300/人。
+3.遊園公車 單程$50，只能坐一次公車
+停車場～沼平車站(藍線車)
+停車場～香林站(紅線車)
+雙程$80，可以坐兩次公車
+藍線來回(停車場～沼平車站)
+紅線來回(停車場～香林站)
+藍紅跨站(停車場～沼平車站～香林站)，簡單說就是可以坐兩次公車，因為您不知道會走到那裡累。
+4.祝山線遊園公車，單程$150(停車場～祝山)
+來回$300 價錢同小火車價錢，凌晨趕不到小火車，只能坐祝山線遊園公車上去囉。
+5.小型車停車費$100。
+玩法：
+1.購票後進入停車場。
+2.前往火車站購票買單程票。
+3.出祝山站往右走，前往小笠原觀景台觀日(步行約10～15分鐘，且為上坡)。
+註一：
+(A)時間來不及者，出祝山車站直行即可觀日(觀日平台不優，有山有樹擋住視野。
+(B)時間充足者，前往小笠原，視野寬闊360度無死角。
+4.觀日完步行下山至沼平車站，慢活下山欣賞早晨風景。
+5.前往沼平車站。
+6.前往阿里山閣飯店旁小徑觀賞，姐妹潭，三兄弟，四姐妹神木。
+7.前往受鎮宮，覓食與休息。
+8.前往阿里山工作站，欣賞櫻花王(野吉櫻花)。
+9.前往三代神木。
+10.前往最高學府(香林國小)。
+11.前往香林神木(2300年)。
+分歧點：
+腳力不足者，退回受鎮宮，塔遊園車($50)回到停車場。
+腳力足者，續往棧道神木一，二區，最後神木車站塔小火車($100)回停車場。
+勇腳者，那就沿路退回工作站，阿里山賓館，停車場囉。
+註二：
+此行程安排從最高點處往下行走，比較不耗體力。
+註三：
+水山神木(2700年),比香林神木(2300年)年齡還大，水山神木步道適合年幼，老者前往，沿路平坦好走，步道入口位於警光山莊處，入口到神木區約行走20分鐘。')
+,('This collection of courses and learning resources aims to improve your technical documentation. Learn how to plan and author technical documents. You can also learn about the role of technical writers at Google.')
+,('google googleg')
+;
+INSERT INTO public.user_file ("content") VALUES
+('Ubisoft terms of useThis Code of Conduct is supplemental to Ubisoft terms of use. Please refer to such terms for additional conduct rules, which can be found here: https://legal.ubi.com/termsofuse/Responsibility:Ubisoft cannot be held responsible for the actions or comments made by Users in the various areas of the game, forums, or other social media outlets. Ubisoft reserves the right to remove any and all content published by a User without prior warning or notification.We strongly advise you not to give personal or account information to anyone. This includes information that can be used to identify you (e.g. social security number, telephone number, physical address) or information that may be used to compromise your account (e.g. personal email address, answers to your security questions).Please note that a representative of Ubisoft will never ask you for your password.Code of Conduct:When you play Tom Clancy''s The Division 2, you must respect other Users and their user rights. The following points must be upheld by everyone and will be enforced by the Ubisoft staff:    Any suspected breach of the Code of Conduct or Terms of Use will lead to investigation by Ubisoft staff. Ubisoft reserves the right to suspend, ban, or delete any User''s account without prior warning or notice.    When using the services provided by Ubisoft, Users may not engage in or encourage any illegal behavior or communications concerning such.    Users will follow all instructions given to them by the Ubisoft staff. Non-compliance of the User regarding any request made by a Ubisoft employee will be considered a breach of the Code of Conduct and may lead to account suspension or revocation.    The User will respect the rules detailed in this Code of Conduct when using the services offered by Ubisoft and the Tom Clancy''s The Division 2 game. The following list of forbidden conduct gives examples of unacceptable behavior but should not be considered exhaustive.Forbidden Conduct    It is forbidden to harass or bully other players via verbal or written communications. Ubisoft will not tolerate any form of harassment and may be obligated to suspend or ban any player account should harassment occur.    It is forbidden to publish pornographic or sexually explicit content via the mediums made available to the player by Ubisoft.    Any language or content deemed illegal, dangerous, threatening, abusive, obscene, vulgar, defamatory, hateful, racist, sexist, ethically offensive or constituting harassment is forbidden. This includes use of the above on the forums, via the in-game chat, over VOIP or other mediums made available in the game.    Impersonation of any other player or Ubisoft employee is strictly forbidden.    Any conduct which interrupts the general flow of gameplay in the game client, forum or any other Ubisoft medium is forbidden. This includes but is not limited to spamming, advertising or soliciting other products.    Verbal or written abuse targeted toward a Ubisoft employee will not be tolerated and may result in account suspension or deletion.    It is forbidden to repost any moderated content on the forums. Please contact a Community Manager directly if you have any questions on why a post or thread was edited or removed.    Exploitation of any new or known bug or glitch for personal gain is strictly forbidden and may result in character rollback, account suspension or revocation.    Any attempt to edit, corrupt or change game or server code is strictly prohibited. Any such behavior will result in the immediate cancellation of the account, and may even give rise to personal liability and/or penal penalties.    Use of third-party hacking, cheating or botting clients is strictly prohibited.Usernames & Clan Names:The following rules clarify the types of names and Clan names that you may not use:    You may not use a name that is contrary to the Code of Conduct. This includes the use of names of a hateful, racist, defamatory, ethnically offensive, obscene, vulgar or sexually explicit nature as well as any other offensive wording.    You may not use names that lead to the harassment or slander of other users.    You may not use the name of any Ubisoft employee.    You may not use a name of a character, object or product covered by copyright or trademark.    You may not use a name from popular culture or the media.    You may not use a name with a religious or historical connotation.    You may not use a name linked to a terrorist organization or activity.    Ubisoft strictly reserves the right to use the names linked to the Tom Clancy''s The Division 2 license.    You may not use a name that refers to an illness, medicine, drug or alcohol.    You may not use any name that breaks any of the aforementioned rules via the association of the first name and surname.    You may not use a name containing spelling mistakes or written in such a way as to break one of the aforementioned rules.Account Security:The User alone is responsible for the security of their account, including protecting their system and account from being compromised. We highly recommend using Two Factor Authentication to help protect your account.Nevertheless, Ubisoft vows to assist the User if they think that their account security is threatened or has been compromised. If an account turns out to have been stolen or used by an unauthorized third party, further investigation will be made by Ubisoft staff, during which time the account will be blocked.Ubisoft does not guarantee compensation for any damages caused by fraudulent use of an account. Only the results of the investigation can determine the necessary action.Any act of account piracy or theft will be punished and may give rise to personal liability or penal penalties for the guilty party.Customer ServiceCustomer Service is there to help you with any problem you may come across in the game. You can contact them via the game or the site, in the support area. Here is a non-exhaustive list of what Customer Service can do for you:    You will receive a quick reply to your questions and problems, depending on the workload of the Customer Service department.    We will do our utmost not to intervene in or interfere with the game unless it is absolutely necessary during our work.    All our Users are treated equally.    Any incident that may be interpreted as breaking the rules will be subject to further investigation. The benefit of the doubt will be given to the User before any action is taken.    All sanctions will be applied as quickly as possible, depending on the seriousness of the breach, the circumstances and your account history.    All correspondence between the User and Customer Service is private. It will not be divulged to other users.Disciplinary Policy:    Ubisoft wishes to offer Users a welcoming game environment, both during gameplay and in the forums. However, a breach of the Code of Conduct may occur. When this happens, Customer Service will investigate further and will make every attempt to be just and fair with the people involved. Depending on the seriousness of the breach and the players'' history, Customer Service may decide to take the following action:    It is possible that there will be no sanctions.    A warning may be sent to the player who has committed the fault. In this case, the action taken will be noted on their account.    A player may have his characters data restored to a previous state if any of his characters gained in-game progress by means that are in violation of this Code of Conduct. The penalty will be noted on their account.    A player may have his user or clan name changed if the name given by the player is in violation of the aforementioned rules. The penalty will be noted on their account.    A player may be suspended for a period of time ranging from a few hours to a few days. The penalty will be noted on their account.    A player may have their game account terminated. This means that their access to the game will be blocked until an unspecified date.Below are examples of behaviour that could lead to your account being banned from playing online or restricting your access in other ways from the game:    Cheating / Modding / Hacking: Players running a modified version of the game client or a third party software providing them with an unfair advantage (wallhacks, aimhacks…) or causing a severe detriment to other players'' experience:        Maximum penalty is permanent ban on first offense    Cheating / Modding / Hacking: Player is running a modified or otherwise unauthorized version of the game client or a third party software providing an unfair advantage (e.g. wallhacks, aimhacks) or causing detriment to other players'' experience:        Maximum penalty is permanent ban    Offensive Language or Behavior/Threats: Posting or publishing (public forums, Clan spaces, private chats or VOIP) any language or content that is in violation of this Code of Conduct        First offense: warning        Second offense: temporary suspension involving in game chat temporary suspension (PC only)        Multiple offenses: Forum Account ban and may result in a permanent ban for severe offenses (PC only)    Harassment: Harassing or serious bullying of other players via verbal or written communications in and outside the game (public forum, private chats or VOIP)        First offense: warning        Second offense: temporary suspension        Multiple offenses: permanent ban    Exploiting: Triggering and using a bug or glitch or bypassing established game rules to gain a significant advantage or skip progression steps otherwise necessary.        First offense: temporary suspension        Multiple offenses: permanent ban    Extent of an account rollback will be decided at Ubisoft''s discretion, on a case by case basis    Duration of a suspension will be decided at Ubisoft''s discretion, on a case by case basis    Actual disciplinary action may differ from the list of examples depending on the severity of the behavior and shall be decided at Ubisoft''s discretion, on a case by case basis    Repeat violations may lead to stricter consequences.Disputing Account Sanctions:If you have a question or wish to dispute an account sanction that you have incurred, please send a request to Customer Service by using the contact options found on our customer support portal. Please be sure to include all relevant information. We will reply to your questions as promptly as possible.Please Respect the Code of Conduct:Help us make this site, the game and the forums a welcoming community space. Respect the Code of Conduct and encourage others to do the same. If you meet players who are not familiar with the Code of Conduct, politely encourage them to read it. ')
+,('大家好 這邊是社長與副社長UBI於昨日晚間6時將有惡意使用BUG玩家隔離7天官方原先在Ubisoft使用條款內有詳細寫出以下規範    任何涉嫌違反《行為準則》或《使用條款》的行為都將導致育碧工作人員進行調查。育碧保留在沒有事先警告或通知的情況下暫停，禁止或刪除任何用戶帳戶的權利。    嚴禁利用任何新的或已知的Bug或故障來謀取個人利益，並可能導致角色回朔，帳戶暫停或吊銷。    如果玩家的任何角色通過違反本《行為準則》的方式在遊戲中取得進展，則可能會將其角色數據恢復到以前的狀態。    利用：觸發和使用錯誤或小故障或繞過已建立的遊戲規則來獲得顯著的優勢，或者跳過其他必要的進展步驟。        初犯：暫時停權        多次犯罪：永久禁止    帳戶回朔的範圍將由Ubisoft自行決定，具體取決於個案    暫停時間將由Ubisoft酌情決定並且本社也讓各位對於此次事件討論了24小時於今日晚間 "XX" 時起 開始進行管制會另開一篇文章讓各位進行留言發洩除外於現時新增個別的抱怨文章或對此事件看法的文章將一律刪除')
+;
